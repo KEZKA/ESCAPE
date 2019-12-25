5ec63c3ba0d1cc75391431aaa3a7857b4a1c6ce0
Sophia <47178761+KEZKA@users.noreply.github.com>
2019-12-25 11:36:51 +0500

    adds class BaseGameProcess

--- ESCAPE/core/base_process.py
+++ ESCAPE/core/base_process.py
@@ -1,1 +1,38 @@
-pass
+import pygame
+
+
+class BaseGameProcess:
+    def __init__(self, game):
+        self.game = game
+        self.is_running = False
+
+    def start(self):
+        self.is_running = True
+        self.execution()
+
+    def stop(self):
+        self.is_running = False
+
+    def execution(self):
+        while self.is_running:
+            for event in pygame.event.get():
+                self._handle_event(event)
+            self.game.clock.tick(self.game.FPS)
+            self.loop()
+            self.render()
+            pygame.display.flip()
+
+    def _handle_event(self, event):
+        if event.type == pygame.QUIT:
+            self.stop()
+            self.game.terminate()
+        self.handle_event()
+
+    def render(self):
+        pass
+
+    def loop(self):
+        pass
+
+    def handle_event(self):
+        pass
