d5b665c9c92af236821ca57ec8ec326f56b8723f
KEZKA <world.is.unpredictable@gmail.com>
2019-12-25 18:31:49 +0500

    adds class BaseGame

---
+++ ESCAPE/core/base_game.py
@@ -0,0 +1,15 @@
+import sys
+import pygame
+
+
+class BaseGame:
+    def __init__(self, width, height):
+        pygame.init()
+        self.width, self.height = width, height
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        self.click = pygame.time.Clock()
+        self.FPS = 60
+
+    def terminate(self):
+        pygame.quit()
+        sys.exit()
