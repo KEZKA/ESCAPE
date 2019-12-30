d9e0f7dd49a90e048976a871e06a6e9564c3e59d
Ilya Barmin <ridering>
2019-12-30 14:20:13 +0600

    adds CrystaSprite

---
+++ ESCAPE/sprites/crystal_sprite.py
@@ -0,0 +1,14 @@
+from ESCAPE.sprites.base_sprite import BaseSprite
+import pygame
+
+
+class CrystalSprite(BaseSprite):
+    def __init__(self):
+        super().__init__()
+        self.append_image(['crystal\crystal_{}.png'.format(number)
+                           for number in range(1, 9)], (100, 100))
+        self.anim_fps = 5
+
+        self.rect = pygame.Rect(0, 0, 100, 100)
+        self.cur_frame = 0
+        self.image = self.sprites[self.cur_frame]
