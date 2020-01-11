3cefea240d906f08b4abe4c893206719230e12a2
ridering <djfgvjbgj@gmail.com>
2020-01-11 19:30:02 +0500

    Adds corridor_sprite

---
+++ ESCAPE/sprites/corridor_sprite.py
@@ -0,0 +1,12 @@
+from ESCAPE.core.base_sprite import BaseSprite
+
+
+class CorridorSprite(BaseSprite):
+    def __init__(self, game, x, y, size=None, angle=0):
+        images = ['images/corridor/corridor_1.png',
+                  'images/corridor/corridor_2.png',
+                  'images/corridor/corridor_3.png']
+
+        super().__init__(game, images, x, y, size, angle)
+        self.add(game.corridor_group)
+        self.anim_fps = 10
