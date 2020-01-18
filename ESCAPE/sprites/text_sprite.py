2d1a71981f5aa17116805227ae8c5584e905fa0f
ridering <djfgvjbgj@gmail.com>
2020-01-18 19:16:08 +0500

    Changes color in text_sprite

--- ESCAPE/sprites/text_sprite.py
+++ ESCAPE/sprites/text_sprite.py
@@ -8,13 +8,13 @@ class TextSprite(BaseSprite):
     def __init__(self, game, x, y, size, text):
         images = []
         self.screen_text = pygame.Surface(size)
-        self.screen_text.fill(pygame.Color('red'))
+        self.screen_text.fill(pygame.Color(102, 217, 255))
         self.interior_rect = (5, 5, size[0] - 10, size[1] - 10)
         self.screen_text.fill(pygame.Color('black'), rect=self.interior_rect)
         super().__init__(game, images, x, y, size, angle=0)
         self.add(game.text_group)
         font = load_font('fonts/game_font.ttf', 40)
-        text = font.render(text, 1, pygame.Color('red'))
+        text = font.render(text, 1, pygame.Color(102, 217, 255))
         self.screen_text.blit(text, ((size[0] - text.get_width()) // 2, 0))
 
     def append_image(self, images, size, angle):
